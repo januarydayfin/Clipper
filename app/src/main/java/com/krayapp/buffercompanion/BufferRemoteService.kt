607d18df64f4a06c1cca8de24b6f04bb7d3f6a72
Tkachuk Sergey <sergey.tkachuk@eltex.loc>
2024-02-05 16:35:45 +0700

    MVP

---
+++ app/src/main/java/com/krayapp/buffercompanion/BufferRemoteService.kt
@@ -0,0 +1,68 @@
+package com.krayapp.buffercompanion
+
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import android.widget.RemoteViewsService.RemoteViewsFactory
+import com.krayapp.buffercompanion.BufferWidgetReceiver.Companion.WIDGET_COPY_ACTION
+
+class BufferRemoteService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory {
+        return ViewsFactory(applicationContext, intent)
+    }
+}
+
+
+class ViewsFactory(private val context: Context, private val intent: Intent?) : RemoteViewsFactory {
+    val prefs = PreferenceManager(context)
+    private val dataList = ArrayList<String>()
+    override fun onCreate() {
+        dataList.clear()
+        dataList.addAll(prefs.getStrings())
+    }
+
+    override fun onDataSetChanged() {
+        dataList.clear()
+        dataList.addAll(prefs.getStrings())
+    }
+
+    override fun onDestroy() {
+
+    }
+
+    override fun getCount(): Int {
+        return dataList.size
+    }
+
+    override fun getViewAt(position: Int): RemoteViews {
+        val remoteView = RemoteViews(context.packageName, R.layout.item_list).apply {
+            setCharSequence(R.id.text, "setText", dataList[position])
+        }
+
+        val intent = Intent(context, BufferWidgetReceiver::class.java).apply {
+            putExtra(WIDGET_COPY_ACTION, dataList[position])
+        }
+
+        remoteView.setOnClickFillInIntent(R.id.itemRoot, intent)
+
+        return remoteView
+    }
+
+    override fun getLoadingView(): RemoteViews {
+        return RemoteViews(context.packageName, R.layout.loading_layout)
+    }
+
+    override fun getViewTypeCount(): Int {
+        return 1
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0L
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+}
--- app/src/main/java/com/krayapp/buffercompanion/BufferWidgetReceiver.kt
+++ app/src/main/java/com/krayapp/buffercompanion/BufferWidgetReceiver.kt
@@ -1,7 +1,6 @@
 package com.krayapp.buffercompanion
 
 import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.app.PendingIntent.FLAG_MUTABLE
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.appwidget.AppWidgetManager
@@ -10,106 +9,65 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
-import android.view.LayoutInflater
 import android.widget.RemoteViews
 import android.widget.RemoteViews.RemoteCollectionItems
 import android.widget.Toast
-import androidx.recyclerview.widget.ListAdapter
-import com.krayapp.buffercompanion.databinding.LayoutMainWidgetScreenBinding
 
 class BufferWidgetReceiver : AppWidgetProvider() {
-	companion object {
-		val WIDGET_COPY_ACTION = "WIDGET_COPY_ACTION"
-		val WIDGET_PASTE_ACTION = "WIDGET_PASTE_ACTION"
-	}
+    companion object {
+        val WIDGET_COPY_ACTION = "WIDGET_COPY_ACTION"
+        val WIDGET_PASTE_ACTION = "WIDGET_PASTE_ACTION"
+    }
 
 
-	override fun onReceive(context: Context, intent: Intent) {
-		super.onReceive(context, intent)
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
 
-		val prefs = PreferenceManager(context)
+        val prefs = PreferenceManager(context)
 
-		if (intent.hasExtra(WIDGET_COPY_ACTION)) {
-			val text = intent.getStringExtra(WIDGET_COPY_ACTION)
-			Log.d("TESTET", String.format("copy %s", text));
-			copy(context, text ?: "")
-		}
-		if (intent.hasExtra(WIDGET_PASTE_ACTION)) {
-			Log.d("TESTET", String.format("%s", getFromClipboard(context)));
-			Toast.makeText(context, getFromClipboard(context), Toast.LENGTH_SHORT).show()
-		}
-	}
+        if (intent.hasExtra(WIDGET_COPY_ACTION)) {
+            val text = intent.getStringExtra(WIDGET_COPY_ACTION)
+            copy(context, text ?: "")
+        }
+        if (intent.hasExtra(WIDGET_PASTE_ACTION)) {
+            Toast.makeText(context, getFromClipboard(context), Toast.LENGTH_SHORT).show()
+        }
+    }
 
-	private fun copy(context: Context, text: String) {
-		val manager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-		val clip = ClipData.newPlainText("label", text)
-		manager.setPrimaryClip(clip)
-	}
+    private fun copy(context: Context, text: String) {
+        val manager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("label", text)
+        manager.setPrimaryClip(clip)
+    }
 
-	private fun getFromClipboard(context: Context): String {
-		val manager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-		return manager.text.toString()
-	}
+    private fun getFromClipboard(context: Context): String {
+        try {
+            val manager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            return manager.text.toString()
+        } catch (e: Exception) {
+            return ""
+        }
+    }
 
-	override fun onUpdate(
-		context: Context,
-		appWidgetManager: AppWidgetManager,
-		appWidgetIds: IntArray
-	) {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        val serviceIntent = Intent(context, BufferRemoteService::class.java)
 
-		val intent = Intent(context, BufferWidgetReceiver::class.java).apply { putExtra(WIDGET_PASTE_ACTION, "") }
-		val views = RemoteViews(
-			context.packageName,
-			R.layout.layout_main_widget_screen
-		).apply {
-			setOnClickPendingIntent(
-				R.id.add, PendingIntent.getBroadcast(
-					context, 0,
-					intent, FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE
-				)
-			)
-			setRemoteAdapter(R.id.listView, getRemoteList(context))
-		}
+        val views = RemoteViews(context.packageName, R.layout.layout_main_widget_screen).apply {
+            setRemoteAdapter(R.id.listView, serviceIntent)
+        }
 
-		appWidgetManager.updateAppWidget(appWidgetIds, views)
-	}
+        val intent = Intent(context, BufferWidgetReceiver::class.java).run {
+            PendingIntent.getBroadcast(context, 0, this, FLAG_UPDATE_CURRENT or FLAG_MUTABLE)
+        }
 
-	private fun getRemoteList(context: Context): RemoteCollectionItems {
-		val remotes = ArrayList<RemoteViews>()
-		val prefs = PreferenceManager(context)
-		for (string in prefs.getStrings()) {
-			remotes.add(
-				RemoteViews(context.packageName, R.layout.item_list).apply {
-					setCharSequence(R.id.text, "setText", string)
-					setOnClickPendingIntent(
-						R.id.copy,
-						PendingIntent.getBroadcast(
-							context,
-							0,
-							Intent(context, BufferWidgetReceiver::class.java).apply {
-								putExtra(
-									WIDGET_COPY_ACTION,
-									string
-								)
-							},
-							FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE
-						)
-					)
-				}
-			)
-		}
-		val collectionBuilder = RemoteCollectionItems.Builder()
-
-		for (remote in remotes) {
-			collectionBuilder.addItem(remote.viewId.toLong(), remote)
-		}
-
-		with(collectionBuilder) {
-			setHasStableIds(true)
-		}
-
-		return collectionBuilder.build()
-	}
+        views.setPendingIntentTemplate(R.id.listView, intent)
+        appWidgetManager.updateAppWidget(appWidgetIds, views)
+    }
 
 }
