2e3c531c0fd95e3559cebc06dc9ef8729cf0649b
Tkachuk Sergey <sergey.tkachuk@eltex.loc>
2024-02-06 13:51:12 +0700

    Добавил крашлитикс

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
 	id("com.google.devtools.ksp") version "1.9.21-1.0.15"
-
+	id("com.google.gms.google-services")
+	id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -51,8 +52,11 @@ dependencies {
 
 	ksp("androidx.room:room-compiler:$room_version")
 	implementation("androidx.core:core-ktx:1.12.0")
+	implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
 	implementation("androidx.appcompat:appcompat:1.6.1")
 	implementation("com.google.android.material:material:1.10.0")
+	implementation("com.google.firebase:firebase-crashlytics")
+	implementation("com.google.firebase:firebase-analytics")
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
