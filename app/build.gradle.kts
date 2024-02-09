315edc9c22a58b4de5a4bd424202cee6739900a3
Tkachuk Sergey <sergey.tkachuk@eltex.loc>
2024-02-09 22:54:08 +0700

    Версия 0.07 ++

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.krayapp.buffercompanion"
 		minSdk = 28
 		targetSdk = 34
-		versionCode = 6
-		versionName = "0.06a"
+		versionCode = 7
+		versionName = "0.07a"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
