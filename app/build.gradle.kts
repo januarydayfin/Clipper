bfa90f100bcb6cb229f6bbfbba7aac9aed155831
Tkachuk Sergey <sergey.tkachuk@eltex.loc>
2024-02-11 15:29:25 +0700

    1.01++

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.krayapp.buffercompanion"
 		minSdk = 28
 		targetSdk = 34
-		versionCode = 100
-		versionName = "1.00"
+		versionCode = 101
+		versionName = "1.01"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
