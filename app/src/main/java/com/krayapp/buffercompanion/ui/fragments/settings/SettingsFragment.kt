ceaa34f8e8fabcc720a11503058135c03c830cf8
Tkachuk Sergey <sergey.tkachuk@eltex.loc>
2024-02-11 14:16:13 +0700

    Добавил перевод и номер версии

--- app/src/main/java/com/krayapp/buffercompanion/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/krayapp/buffercompanion/ui/fragments/settings/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.krayapp.buffercompanion.ui.fragments.settings
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import com.krayapp.buffercompanion.ClipperApp
 import com.krayapp.buffercompanion.R
 import com.krayapp.buffercompanion.activity
 import com.krayapp.buffercompanion.databinding.FragmentSettingsBinding
+import com.krayapp.buffercompanion.setVisible
 import com.krayapp.buffercompanion.ui.fragments.AbsFragment
 
 class SettingsFragment : AbsFragment() {
@@ -26,9 +29,28 @@ class SettingsFragment : AbsFragment() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+
+		vb.version.text = getAppVersion()
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+			vb.dynamicColorsSwitch.setVisible()
+
 		initClick()
 	}
 
+	private fun getAppVersion(): String {
+		return try {
+			val packageManager = requireContext().packageManager
+			val packageName = requireContext().packageName
+			val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+				packageManager.getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(0))
+			} else {
+				packageManager.getPackageInfo(packageName, 0)
+			}
+			packageInfo.versionName
+		} catch (e: Exception) {
+			""
+		}
+	}
 	override fun getTitleRes(): Int {
 		return R.string.settings_title
 	}
