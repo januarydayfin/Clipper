b039fdbdc160f6667324b520cf142dec5f5e1401
Sergey Tkachuk <sergey.tkachuk@eltex.loc>
2024-09-18 21:51:36 +0300

    Отключил аналитику google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
 	id("com.google.devtools.ksp") version "1.9.21-1.0.15"
-	id("com.google.gms.google-services")
-	id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -57,8 +55,6 @@ dependencies {
 	implementation("androidx.appcompat:appcompat:1.6.1")
 	implementation("androidx.recyclerview:recyclerview:1.3.2")
 	implementation("com.google.android.material:material:1.11.0")
-	implementation("com.google.firebase:firebase-crashlytics")
-	implementation("com.google.firebase:firebase-analytics")
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
