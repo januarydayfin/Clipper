c5f05105b8ee154fd1a446c27c914e58025f87f6
Sergey Tkachuk <sergey.tkachuk@eltex.loc>
2025-07-17 08:13:15 +0300

    Повышение версий зависимостей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
-	id("com.google.devtools.ksp") version "1.9.21-1.0.15"
+	id("com.google.devtools.ksp") version "2.0.21-1.0.25"
 }
 
 android {
 	namespace = "com.krayapp.buffercompanion"
-	compileSdk = 34
+	compileSdk = 35
 
 	defaultConfig {
-		applicationId = "com.krayapp.buffercompanion"
+		applicationId = "com.krayapp.buffercompanion.bargen"
 		minSdk = 28
-		targetSdk = 34
+		targetSdk = 35
 		versionCode = 109
 		versionName = "1.09"
 
@@ -41,21 +41,21 @@ android {
 }
 
 dependencies {
-	implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-	implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-	val room_version = "2.6.1"
-	implementation("androidx.activity:activity-ktx:1.8.2")
-	implementation("androidx.room:room-runtime:$room_version")
-	annotationProcessor("androidx.room:room-compiler:$room_version")
-	implementation("androidx.room:room-ktx:$room_version")
+	val roomVersion = "2.7.2"
+	implementation("androidx.navigation:navigation-fragment-ktx:2.9.2")
+	implementation("androidx.navigation:navigation-ui-ktx:2.9.2")
+	implementation("androidx.activity:activity-ktx:1.10.1")
+	implementation("androidx.room:room-runtime:$roomVersion")
+	annotationProcessor("androidx.room:room-compiler:$roomVersion")
+	implementation("androidx.room:room-ktx:$roomVersion")
 
-	ksp("androidx.room:room-compiler:$room_version")
-	implementation("androidx.core:core-ktx:1.12.0")
-	implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
-	implementation("androidx.appcompat:appcompat:1.6.1")
-	implementation("androidx.recyclerview:recyclerview:1.3.2")
-	implementation("com.google.android.material:material:1.11.0")
+	ksp("androidx.room:room-compiler:$roomVersion")
+	implementation("androidx.core:core-ktx:1.16.0")
+	implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+	implementation("androidx.appcompat:appcompat:1.7.1")
+	implementation("androidx.recyclerview:recyclerview:1.4.0")
+	implementation("com.google.android.material:material:1.12.0")
 	testImplementation("junit:junit:4.13.2")
-	androidTestImplementation("androidx.test.ext:junit:1.1.5")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+	androidTestImplementation("androidx.test.ext:junit:1.2.1")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
