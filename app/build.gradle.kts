154c2ae4b7ff410203ddfbad99b1172ae70ced8a
Sergey Tkachuk <sergey.tkachuk@eltex.loc>
2025-08-29 10:10:36 +0300

    ver++ 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.krayapp.buffercompanion.bargen"
         minSdk = 30
         targetSdk = 35
-        versionCode = 210
-        versionName = "2.1.0"
+        versionCode = 211
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
