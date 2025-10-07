d9e50b301e773bbff12290cd9a03e49f4bce2c6d
Sergey Tkachuk <sergey.tkachuk@eltex.loc>
2025-10-07 11:34:12 +0300

    Фикс фильтра по тегам

--- app/src/main/java/com/krayapp/buffercompanion/bargen/domain/pagingSource/BarcodeFilterTagsPagingSource.kt
+++ app/src/main/java/com/krayapp/buffercompanion/bargen/domain/pagingSource/BarcodeFilterTagsPagingSource.kt
@@ -3,22 +3,41 @@ package com.krayapp.buffercompanion.bargen.domain.pagingSource
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.krayapp.buffercompanion.bargen.data.room.bargen.BargenDB
-import com.krayapp.buffercompanion.bargen.data.room.bargen.entity.BarcodeEntity
+import com.krayapp.buffercompanion.bargen.data.room.entity.BarcodeEntity
 import com.krayapp.buffercompanion.bargen.domain.provideDatabase
 
 class BarcodeFilterTagsPagingSource(private val tagIds: List<String>) :
     PagingSource<Int, BarcodeEntity>() {
     private val barcodeDao = provideDatabase<BargenDB>().barcodeDao()
-
-    override fun getRefreshKey(state: PagingState<Int, BarcodeEntity>): Int = 0
+    override fun getRefreshKey(state: PagingState<Int, BarcodeEntity>): Int? = null
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, BarcodeEntity> {
-        val pagingNextKey = params.key ?: 0
-        val page = barcodeDao.loadPage(pageSize = PAGE_SIZE, offset = pagingNextKey)
-        val filtered = page.filter { it.tags.containsAll(tagIds) }
+        val allCount = barcodeDao.count()
+        var offset = params.key ?: 0
+        val resultList = mutableListOf<BarcodeEntity>()
+        var skippedPageItemsCount = 0
 
-        val nextKey = if (page.isEmpty()) null else pagingNextKey + PAGE_SIZE
-        return LoadResult.Page(data = filtered, nextKey = (nextKey), prevKey = nextKey)
+        while (resultList.size < PAGE_SIZE) {
+            barcodeDao.loadPage(pageSize = PAGE_SIZE, offset).filter {
+                it.tags.containsAll(tagIds)
+            }.onEach {
+                if (resultList.size < PAGE_SIZE)
+                    resultList.add(it)
+                else
+                    skippedPageItemsCount++
+            }
+
+            offset += (PAGE_SIZE - skippedPageItemsCount)
+
+            if (offset >= allCount)
+                break
+        }
+
+        return LoadResult.Page(
+            data = resultList,
+            nextKey = if (offset >= allCount) null else offset,
+            prevKey = null
+        )
     }
 
     companion object {
