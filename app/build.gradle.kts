1ed80ab4fe84659527751330c47407bc292c08ac
Sergey Tkachuk <kraymandev@gmail.com>
2026-06-14 11:36:11 +0300

    ver++ 3.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.krayapp.buffercompanion.bargen"
         minSdk = 30
         targetSdk = 37
-        versionCode = 333
-        versionName = "3.3.3"
+        versionCode = 334
+        versionName = "3.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
